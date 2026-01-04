1d899ee10363866497aa88325d72864de0e7ee64
B. KANTE VENKATA ANURAG <anuragnakul2702@gmail.com>
2026-01-04 23:31:59 +0530

    chore: v0.1.4, add MIT license, format configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 // Tailwind CSS configuration for CloudSniper UI components
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html', 
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
